e4da090a0b0cbe1f35fbde805aebbc61db3ef115
Kerri Miller <kerrizor@github.com>
2017-02-16 13:34:19 -0800

    Spoof in test code from bundler

--- lib/kw_pony.rb
+++ lib/kw_pony.rb
@@ -2,11 +2,17 @@ require "kw_pony/version"
 
 module KwPony
   # Your code goes here...
-  class Pony < Bundler::Plugin::API
-    command "say", self
+  class Pony# < Bundler::Plugin::API
+    # command "say", self
+    #
+    # def exec(command, args)
+    #   puts "pony!"
+    # end
+
+    Bundler::Plugin::API.command "echo", self
 
     def exec(command, args)
-      puts "pony!"
+      puts "You gave me \#{args.join(", ")}"
     end
   end
 end
