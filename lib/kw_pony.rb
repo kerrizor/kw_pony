beb284ef4e497878aad898abad8fbf9f7cd18c27
Kerri Miller <kerrizor@github.com>
2017-02-16 18:04:20 -0800

    Revert "Remove everything"
    This reverts commit 68cb8135f3ed111caddb4b6b753293bd9a546519.

---
+++ lib/kw_pony.rb
@@ -0,0 +1,18 @@
+require "kw_pony/version"
+
+module KwPony
+  # Your code goes here...
+  class Pony# < Bundler::Plugin::API
+    # command "say", self
+    #
+    # def exec(command, args)
+    #   puts "pony!"
+    # end
+
+    Bundler::Plugin::API.command "echo", self
+
+    def exec(command, args)
+      puts "You gave me \#{args.join(", ")}"
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'kw_pony'
+
+require 'minitest/autorun'
